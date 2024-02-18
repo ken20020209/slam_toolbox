ccd2194cc878f514a72458bd5197e5852236dbb2
ken20020209 <ken20020209@gmail.com>
2024-02-18 16:57:15 +0800

    Add params_file launch argument to localization_launch.py

--- launch/localization_launch.py
+++ launch/localization_launch.py
@@ -1,13 +1,22 @@
 from launch import LaunchDescription
 import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
-
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.actions import DeclareLaunchArgument, LogInfo
 
 def generate_launch_description():
+    params_file = LaunchConfiguration('params_file', default=get_package_share_directory("slam_toolbox") + '/config/mapper_params_localization.yaml')
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=params_file,
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
+
     return LaunchDescription([
+        declare_params_file_cmd,
         launch_ros.actions.Node(
           parameters=[
-            get_package_share_directory("slam_toolbox") + '/config/mapper_params_localization.yaml'
+            params_file
           ],
           package='slam_toolbox',
           executable='localization_slam_toolbox_node',
